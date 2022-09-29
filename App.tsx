81a3a4e11479dfb6175758d574e0233f7f11c229
latif <latifissaka069@gmail.com>
2022-09-29 10:34:03 +0000

    new commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useRef } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import BottomSheet, { BottomSheetRefProps } from './components/BottomSheet';
 
 export default function App() {
+  const ref = useRef<BottomSheetRefProps>(null)
+
+  const onPress = useCallback(() => {
+    const isActive = ref?.current?.isActive();
+    if (isActive) {
+      ref?.current?.scrollTo(0);
+    } else {
+      ref?.current?.scrollTo(-200);
+    }
+  }, []);
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <TouchableOpacity style={styles.button} onPress={onPress} />
+        <BottomSheet ref={ref}>
+          <View style={styles.bottomSheetView}>
+            <Text style={{fontSize: 24}}>This is a bottom sheet</Text>
+          </View>
+        </BottomSheet>
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#111',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    height: 50,
+    borderRadius: 25,
+    aspectRatio: 1,
+    backgroundColor: 'white',
+    opacity: 0.6,
+  },
+  bottomSheetView: {
+    flex: 1,
+    backgroundColor: 'orange',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
